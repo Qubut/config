8d55be6b4b8c9c957b9ee1f96c5f70d63d75c787
Qubut <s-aahmed@haw-landshut.de>
2025-04-13 14:29:07 +0200

    feat(sys): create networking module

---
+++ system/networking/default.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, systemSettings, ... }:
+{
+  networking.hostName = systemSettings.hostName; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
+  networking.firewall.allowedTCPPorts = [ ];
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp47s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp46s0.useDHCP = lib.mkDefault true;
+}
