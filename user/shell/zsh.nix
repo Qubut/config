d2d5824d3d7a9c44eca5e4022139aca7aeee5737
Qubut <s-aahmed@haw-landshut.de>
2025-04-13 14:55:15 +0200

    refactor(user/shell): rename sh.nix to zsh.nix and move common shell config to default.nix

---
+++ user/shell/zsh.nix
@@ -0,0 +1,53 @@
+{
+  config,
+  pkgs,
+  userSettings,
+  ...
+}:
+let
+  aliases = builtins.readFile ./zsh-aliases;
+  functions = builtins.readFile ./zsh-functions;
+  xdgStateHome = builtins.getEnv "XDG_STATE_HOME";
+  p10k = builtins.readFile ./p10k.zsh;
+in
+{
+  programs.zsh = {
+    enable = true;
+    prezto = {
+      enable = true;
+      pmodules = [
+        "editor"
+        "history"
+        "completion"
+        "prompt"
+        "syntax-highlighting"
+        "autosuggestions"
+        "directory"
+        "utility"
+        "git"
+        "history-substring-search"
+        "tmux"
+        "ssh"
+        "spectrum"
+      ];
+      prompt.theme = "powerlevel10k";
+      tmux.autoStartLocal = true;
+      tmux.defaultSessionName = "autostart";
+      extraConfig = ''
+        zstyle ':prezto:module:history-substring-search' unique 'yes'
+      '';
+    };
+    history = {
+      size = 10000;
+      path = "${config.xdg.stateHome}/zsh/history";
+      extended = true;
+      ignoreSpace = true;
+    };
+    dotDir = ".config/zsh";
+    initExtra = ''
+      ${aliases}
+      ${functions}
+      ${p10k}
+    '';
+  };
+}
