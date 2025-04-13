49ee6580544bcdc049473521df3f7508be90d423
Qubut <s-aahmed@haw-landshut.de>
2025-04-13 14:28:24 +0200

    feat(sys): systemd - handle out of memory error

--- system/hardware/systemd.nix
+++ system/hardware/systemd.nix
@@ -4,4 +4,18 @@
   services.journald.extraConfig = "SystemMaxUse=50M\nSystemMaxFiles=5";
   services.journald.rateLimitBurst = 500;
   services.journald.rateLimitInterval = "30s";
+  systemd = {
+    slices."nix-daemon".sliceConfig = {
+      ManagedOOMMemoryPressure = "kill";
+      ManagedOOMMemoryPressureLimit = "50%";
+    };
+    services."nix-daemon" = {
+      serviceConfig = {
+        Slice = "nix-daemon.slice";
+        OOMScoreAdjust = 1000; # Prefer killing build processes
+        MemoryHigh = "80%";
+        MemoryMax = "90%";
+      };
+    };
+  };
 }
