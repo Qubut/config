d3b91229a5357d51fb88ec8d58cc88dd2cfadfe6
Qubut <s-aahmed@haw-landshut.de>
2025-04-13 14:35:19 +0200

    user(apps): vscodium - add some needed plugins and define some default behavior

--- user/apps/vscodium.nix
+++ user/apps/vscodium.nix
@@ -7,12 +7,24 @@
     extensions = with pkgs.vscode-extensions; [
       dracula-theme.theme-dracula
       vscodevim.vim
+      vscode-icons-team.vscode-icons
+      mkhl.direnv
+      jnoortheen.nix-ide
+      b4dm4n.vscode-nixpkgs-fmt
+      tamasfe.even-better-toml
       ms-vscode-remote.remote-containers
       ms-azuretools.vscode-docker
+      ms-python.vscode-pylance
+      ms-python.python
+      ms-python.debugpy
+      ms-python.pylint
+      # pylyzer.pylyzer
+      ms-python.isort
+      ms-python.black-formatter
       bbenoist.nix
       scalameta.metals
       scala-lang.scala
-      # pkgs.vscode-extensions.haskell.haskell
+      haskell.haskell
       denoland.vscode-deno
     ];
     haskell = {
@@ -23,6 +35,8 @@
       };
     };
     userSettings = {
+      "workbench.iconTheme" = "vscode-icons";
+      "python.linting.pylintArgs" = [ "--extension-pkg-whitelist=cv2" "--generate-members" ];
       "svelte-enable-ts-plugin" = true;
       "languageServerHaskell.enableHIE" = true;
       "languageServerHaskell.hieExecutablePath" = "${pkgs.haskellPackages.implicit-hie}/bin/gen-hie";
@@ -33,10 +47,30 @@
       "files.trimFinalNewlines" = true;
       "files.insertFinalNewline" = true;
       "diffEditor.ignoreTrimWhitespace" = false;
+      "jupyter.askForKernelRestart" = false;
       "workbench.editorAssociations" = {
         "{git,gitlens,git-graph}:/**/*.{md,csv,svg}" = "default";
       };
       "explorer.fileNesting.enabled" = true;
+      "explorer.fileNesting.patterns" = {
+        "*.ts" = "\${capture}.js, \${capture}.typegen.ts";
+        "*.js" = "\${capture}.js.map, \${capture}.min.js, \${capture}.d.ts";
+        "*.jsx" = "\${capture}.js";
+        "*.tsx" = "\${capture}.ts, \${capture}.typegen.ts";
+        "tsconfig.json" = "tsconfig.*.json";
+        "package.json" = "package-lock.json, yarn.lock, pnpm-lock.yaml, bun.lockb";
+        "*.sqlite" = "\${capture}.\${extname}-*";
+        "*.db" = "\${capture}.\${extname}-*";
+        "*.sqlite3" = "\${capture}.\${extname}-*";
+        "*.db3" = "\${capture}.\${extname}-*";
+        "*.sdb" = "\${capture}.\${extname}-*";
+        "*.s3db" = "\${capture}.\${extname}-*";
+        "*.mts" = "\${capture}.typegen.ts";
+        "*.cts" = "\${capture}.typegen.ts";
+      };
+      "[nix]" = {
+        "editor.defaultFormatter" = "B4dM4n.nixpkgs-fmt";
+      };
     };
   };
 }
