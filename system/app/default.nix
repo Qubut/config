2ab79421654bcf81163e92d2fd57584e4415c97b
Qubut <s-aahmed@haw-landshut.de>
2025-04-13 14:27:24 +0200

    feat(sys): create app module

---
+++ system/app/default.nix
@@ -0,0 +1,20 @@
+{...}:
+{
+    imports = [
+    (import ./docker.nix {
+      storageDriver = null;
+      inherit pkgs userSettings lib;
+    })
+    ./waydroid.nix
+    ./virtualization.nix
+    ./gamemode.nix
+    ./steam.nix
+  ];
+  programs.xfconf.enable = true;
+  programs.thunar.enable = true;
+  programs.nm-applet.enable = true;
+  programs.thunar.plugins = with pkgs.xfce; [
+    thunar-archive-plugin
+    thunar-volman
+  ];
+}
