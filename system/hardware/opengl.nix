b0ffa8be9a7bcb39eacd394f3247902382eeb66e
Qubut <s-aahmed@haw-landshut.de>
2026-01-05 10:56:03 +0100

    feat(sys:hardware): add some gpu drivers

--- system/hardware/opengl.nix
+++ system/hardware/opengl.nix
@@ -1,10 +1,17 @@
-{ pkgs, ... }:
-
+{ pkgs, systemSettings, ... }:
+let
+  cpuType = systemSettings.cpuType;
+  intel-media-drivers = if cpuType == "intel" then [ pkgs.intel-media-driver ] else [];
+in
 {
   hardware.graphics.enable = true;
   hardware.graphics.enable32Bit = true;
   hardware.graphics.extraPackages = with pkgs; [
     rocmPackages.clr.icd
-    intel-media-driver
-  ];
+    libvdpau-va-gl
+  ] ++ intel-media-drivers;
+
+  environment.sessionVariables =
+    if cpuType == "intel" then { LIBVA_DRIVER_NAME = "iHD"; }
+    else {};
 }
