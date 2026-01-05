7a0f9d88d0c0fa801ed801f9592fcd388955ba85
Qubut <s-aahmed@haw-landshut.de>
2026-01-05 10:54:10 +0100

    chore: make hyprland wm, consider if cpu has a gpu and add atuin server port

--- constants.nix
+++ constants.nix
@@ -8,19 +8,22 @@
     grubDevice = ""; # Only used for legacy (BIOS) boot mode
     gpuType = "nvidia"; # Supports slight mods for AMD currently
     cpuType = "intel";
+    cpuHasGpu = true;
   };
   userSettings = rec {
     username = "falcon";
     name = "Falcon";
     dotfilesDir = "~/.dotfiles";
     theme = "ayu-dark";
-    wm = "qtile"; # Must match selections in ./user/wm/ and ./system/wm/
+    wm = "hyprland"; # Must match selections in ./user/wm/ and ./system/wm/
     wmType = if (wm == "hyprland") then "wayland" else "x11";
+    dm = "sddm";
     browser = "firefox"; # Must match selection in ./user/app/browser/
     term = "kitty";
     font = "DejaVu Sans Mono";
     fontPkg = pkgs.dejavu_fonts;
     editor = "helix";
+    atuinServerPort = 8888;
   };
   machines = [
     { hostname = "snowfire"; profile = "static"; }
