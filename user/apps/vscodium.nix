1df2e701c5539fede79959103016b49ab01c498c
Qubut <s-aahmed@haw-landshut.de>
2026-01-05 11:07:34 +0100

    feat(user:apps/vscodium): add and configure some plugins

--- user/apps/vscodium.nix
+++ user/apps/vscodium.nix
@@ -1,6 +1,13 @@
 { pkgs, ... }:
 
 {
+  home.packages =
+    with pkgs;
+    [
+      ruff
+      pyright
+      # pylyzer
+    ];
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
@@ -8,24 +15,41 @@
       dracula-theme.theme-dracula
       vscodevim.vim
       vscode-icons-team.vscode-icons
+      pkief.material-icon-theme
       mkhl.direnv
       jnoortheen.nix-ide
       b4dm4n.vscode-nixpkgs-fmt
       tamasfe.even-better-toml
       ms-vscode-remote.remote-containers
       ms-azuretools.vscode-docker
-      ms-python.vscode-pylance
+      charliermarsh.ruff
       ms-python.python
       ms-python.debugpy
-      ms-python.pylint
-      # pylyzer.pylyzer
       ms-python.isort
       ms-python.black-formatter
+      # ms-python.vscode-pylance
+      ms-python.mypy-type-checker
+      ms-toolsai.jupyter
+      ms-toolsai.jupyter-keymap
+      ms-toolsai.jupyter-renderers
+      ms-toolsai.vscode-jupyter-cell-tags
+      ms-toolsai.vscode-jupyter-slideshow
+      ms-pyright.pyright
+      rust-lang.rust-analyzer
+      fill-labs.dependi
+      ms-azuretools.vscode-docker
+      ms-vscode-remote.remote-containers
+      redhat.vscode-yaml
+      hashicorp.terraform
       bbenoist.nix
+      arrterian.nix-env-selector
       scalameta.metals
       scala-lang.scala
       haskell.haskell
       denoland.vscode-deno
+      codezombiech.gitignore
+      ms-vscode-remote.remote-ssh
+      ms-vscode-remote.remote-ssh-edit
     ];
     haskell = {
       enable = true;
@@ -37,8 +61,19 @@
     profiles.default.userSettings = {
       "typst-lsp.experimentalFormatterMode" = "on";
       "typst-lsp.serverPath" = "${pkgs.tinymist}/bin/tinymist";
-      "workbench.iconTheme" = "vscode-icons";
-      "python.linting.pylintArgs" = [ "--extension-pkg-whitelist=cv2" "--generate-members" ];
+      "workbench.iconTheme" = "material-icon-theme";
+      "python.defaultInterpreterPath" = "\${workspaceFolder}/.devenv/state/venv/bin/python";
+      "python.analysis.extraPaths" = [ "\${workspaceFolder}/src" ];
+      "python.languageServer" = "Pyright";
+      "python.analysis.autoImportCompletions" = true;
+      "python.analysis.autoSearchPaths" = true;
+      "python.analysis.inlayHints.functionReturnTypes" = true;
+      "python.analysis.inlayHints.variableTypes" = true;
+      "python.analysis.nodeExecutable" = "${pkgs.nodejs_24}/bin/node";
+      "python.analysis.typeCheckingMode" = "standard";
+      "python.analysis.completeFunctionParens" = true;
+      "python.linting.enabled" = true;
+      "python.linting.ruffEnabled" = true;
       "svelte-enable-ts-plugin" = true;
       "languageServerHaskell.enableHIE" = true;
       "languageServerHaskell.hieExecutablePath" = "${pkgs.haskellPackages.implicit-hie}/bin/gen-hie";
@@ -73,6 +108,9 @@
       "[nix]" = {
         "editor.defaultFormatter" = "B4dM4n.nixpkgs-fmt";
       };
+      "rust-analyzer.linkedProjects" = [ "\${workspaceFolder}/Cargo.toml" ];
+      "rust-analyzer.check.command" = "clippy";
+      "rust-analyzer.rustc.source" = "discover";
     };
   };
 }
