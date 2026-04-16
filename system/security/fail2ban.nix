9d83e03b1c307bd2887925109e2cbb5e63337213
Qubut <s-aahmed@haw-landshut.de>
2026-04-16 16:33:39 +0200

    security: correct settings

--- system/security/fail2ban.nix
+++ system/security/fail2ban.nix
@@ -3,9 +3,6 @@
 {
   services.fail2ban = {
     enable = true;
-    maxretry = 5;
-    findtime = "10m";
-    bantime = "1h";
     ignoreIP = [
       "127.0.0.0/8"
       "::1"
@@ -13,5 +10,10 @@
       "172.16.0.0/12"
       "192.168.0.0/16"
     ];
+    jails.sshd.settings = {
+      maxretry = 5;
+      findtime = "10m";
+      bantime = "1h";
+    };
   };
 }
